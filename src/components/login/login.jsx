17148b247b10b9e527fe1fe21ded5aeec99f56c9
Mario343214420 <343214420@qq.com>
2018-12-03 20:37:24 +0800

    now：完成登录界面，next：

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,11 +1,72 @@
 import React, {Component} from 'react';
+import {NavBar, WingBlank, WhiteSpace, List, InputItem, Button} from 'antd-mobile';
+import Logo from '../logo';
 
-class Login extends Component {
-  render () {
+class Register extends Component {
+  state = {
+    userName:'',
+    password:'',
+  }
+  // changeRadio = type => {
+  //   if (type === 'boss') {
+  //     this.setState({
+  //       selected: true
+  //     })
+  //   } else {
+  //     this.setState({
+  //       selected: false
+  //     })
+  //   }
+  // }
+  
+  // getUseName =(value)=>{
+  //   this.setState({
+  //     username:value,
+  //   })
+  // }
+  //
+  // getPasswprd =(value)=>{
+  //   this.setState({
+  //     password:value,
+  //   })
+  // }
+  
+  getInMsg = (type,value)=>{
+    this.setState({
+      [type]:value
+      
+    })
+  }
+  
+  register =()=>{
+    const {password,userName}=this.state;
+    console.log( password, userName);
+  }
+  goRegister =()=>{
+    this.props.history.replace('/register');
+  }
+  render() {
+    
     return (
-      <h1>登录页面</h1>
+      <div>
+        <NavBar>硅谷直聘</NavBar>
+        <Logo/>
+        <WingBlank>
+          <List>
+            <InputItem onChange={val => this.getInMsg('userName',val)}>用&nbsp;&nbsp;户&nbsp;&nbsp;名：</InputItem>
+            <WhiteSpace/>
+            <InputItem onChange={val => this.getInMsg('password',val)}>密&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;码：</InputItem>
+            
+            <WhiteSpace/>
+            <Button type="primary" onClick={this.register}>登录</Button>
+            <WhiteSpace/>
+            <Button onClick={this.goRegister}>注册账号</Button>
+            <WhiteSpace/>
+          </List>
+        </WingBlank>
+      </div>
     )
   }
 }
 
-export default Login;
+export default Register;
