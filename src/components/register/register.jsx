fba47384d7ce8e4ed40b159aa3bbea03b412da22
Mario343214420 <343214420@qq.com>
2018-12-03 20:31:17 +0800

    now：注册页面完成，next：实现登录页面

--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -1,30 +1,76 @@
 import React, {Component} from 'react';
-import {NavBar, WingBlank, WhiteSpace, List, InputItem, Radio,Button} from 'antd-mobile';
+import {NavBar, WingBlank, WhiteSpace, List, InputItem, Radio, Button} from 'antd-mobile';
 import Logo from '../logo';
 const Item = List.Item;
 class Register extends Component {
+  state = {
+    boss:true,
+    selected: true,
+    userName:'',
+    password:'',
+    rePassword:'',
+  }
+  // changeRadio = type => {
+  //   if (type === 'boss') {
+  //     this.setState({
+  //       selected: true
+  //     })
+  //   } else {
+  //     this.setState({
+  //       selected: false
+  //     })
+  //   }
+  // }
+  
+  getUseName =(value)=>{
+    this.setState({
+      username:value,
+    })
+  }
+  
+  getPasswprd =(value)=>{
+    this.setState({
+      password:value,
+    })
+  }
+  
+  getInMsg = (type,value)=>{
+    this.setState({
+      [type]:value
+      
+    })
+  }
+  
+  register =()=>{
+    const {boss,password,rePassword,userName}=this.state;
+    console.log(boss, password, rePassword, userName);
+  }
+  goLogin =()=>{
+    this.props.history.replace('/login');
+  }
   render() {
+    const {boss} = this.state;
     return (
       <div>
         <NavBar>硅谷直聘</NavBar>
         <Logo/>
         <WingBlank>
           <List>
-            <InputItem>用户名：</InputItem>
+            <InputItem onChange={val => this.getInMsg('userName',val)}>用&nbsp;&nbsp;户&nbsp;&nbsp;名：</InputItem>
             <WhiteSpace/>
-            <InputItem>用户名：</InputItem>
+            <InputItem onChange={val => this.getInMsg('password',val)}>密&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;码：</InputItem>
             <WhiteSpace/>
-            <InputItem>用户名：</InputItem>
+            <InputItem onChange={val => this.getInMsg('rePassword',val)}>确认密码：</InputItem>
             <WhiteSpace/>
             <Item>
               用户类型：&nbsp;&nbsp;&nbsp;&nbsp;
-              <Radio>求职</Radio>&nbsp;&nbsp;&nbsp;&nbsp;
-              <Radio>招聘</Radio>
+              <Radio checked={!boss} onChange={this.getInMsg.bind(null, 'boss',false)}>求职</Radio>&nbsp;&nbsp;&nbsp;&nbsp;
+              <Radio checked={boss} onChange={this.getInMsg.bind(null, 'boss',true)}>招聘</Radio>
             </Item>
             <WhiteSpace/>
-            <Button type="primary">注册</Button>
+            <Button type="primary" onClick={this.register}>注册</Button>
             <WhiteSpace/>
-            <Button>已有账户</Button>
+            <Button onClick={this.goLogin}>已有账户</Button>
             <WhiteSpace/>
           </List>
         </WingBlank>
